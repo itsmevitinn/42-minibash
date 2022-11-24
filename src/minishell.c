ca718ad1591c79945097604ae3c762df75e79d4f
Vitor Marcondes <vitorsergio111@gmail.com>
2022-11-24 13:28:21 -0300

    feat: finished prompt msg & fixed leaks

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: vsergio <vsergio@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/22 11:04:21 by vsergio           #+#    #+#             */
-/*   Updated: 2022/11/23 18:41:35 by vsergio          ###   ########.fr       */
+/*   Updated: 2022/11/24 13:27:18 by vsergio          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,46 +14,74 @@
 
 int main(void)
 {
-	char *current_dir;
-	char *logname;
-	int cmd_pipe[2];
+	t_prompt prompt_msg;
 	char *user_input;
-	char *prompt_msg;
+
+	get_prompt_msg(&prompt_msg);
+
+	//logname are not mallocated
+	prompt_msg.logname = ft_strjoin(prompt_msg.logname, "@", 0);
+	prompt_msg.final_msg = ft_strjoin(prompt_msg.logname, prompt_msg.hostname, 'a');
+	prompt_msg.final_msg = ft_strjoin(prompt_msg.final_msg, ":", 1);
+	prompt_msg.final_msg = ft_strjoin(prompt_msg.final_msg, prompt_msg.pwd, 'a');
+	prompt_msg.final_msg = ft_strjoin(prompt_msg.final_msg, "$ ", 1);
 	
-	char *hostname;
-	int child_hostname;
-
-	pipe(cmd_pipe);
-	//Variable that will receive value from read
-	hostname = malloc(100);
-	//Returns the current directory (pwd) from terminal
-	current_dir = getcwd(NULL, 0);
-	printf("Current dir: %s\n", current_dir);
-	
-	//Need do this malloc dinamically
-	logname = malloc(100);
-	//Returns the value of a variable from environment
-	logname = getenv("LOGNAME");
-	printf("Logname: %s\n", logname);
-	
-
-	child_hostname = fork();
-	if (child_hostname == 0)
-		get_hostname(cmd_pipe);
-	waitpid(child_hostname, NULL, 0);
-	//Read the hostname from pipe and send to variable "hostname"
-	read(cmd_pipe[0], hostname, 6);
-
-
-	logname = ft_strjoin(logname, "@");
-	prompt_msg = ft_strjoin(logname, hostname);
 	//print a prompt message and waits for user input from terminal
-	user_input = readline(prompt_msg);
+	user_input = readline(prompt_msg.final_msg);
 	printf("Input from terminal: %s\n", user_input);
+	free(prompt_msg.final_msg);
 }
 
-void	get_hostname(int *cmd_output)
+void	get_hostname(int *pipe_hostname)
 {
-	dup2(cmd_output[1], 1);
+	dup2(pipe_hostname[1], 1);
 	execve("/bin/hostname", NULL, NULL);
+}
+
+void	get_prompt_msg(t_prompt *prompt_msg)
+{
+	int pipe_hostname[2];
+	int child_hostname;
+
+	//malloc len accordingly to 42 hostname's
+	prompt_msg->hostname = malloc(6);
+	
+	//Receive the value of a environment variable (with malloc)
+	prompt_msg->logname = getenv("LOGNAME");
+	
+	//Variable that will receive value from read
+	pipe(pipe_hostname);
+	//Process that will run execve and write the hostname in pipe
+	child_hostname = fork();
+	if (child_hostname == 0)
+		get_hostname(pipe_hostname);
+	//Wait untill child processs end
+	waitpid(child_hostname, NULL, 0);
+	//Read the hostname from pipe and send to variable "hostname"
+	read(pipe_hostname[0], prompt_msg->hostname, 6);
+	get_current_dir(prompt_msg);
+}
+
+void	get_current_dir(t_prompt *prompt_msg)
+{
+	char	*home;
+	char	*current_dir;
+	int		home_len;
+
+	//receive the actual directory
+	current_dir = getcwd(NULL, 0);
+	//this is equal to "/Users/vsergio"
+	home = ft_strjoin("/Users/", prompt_msg->logname, 0);
+	home_len = ft_strlen(home);
+
+	//check if the current dir have "/Users/username"
+	if (ft_strncmp(current_dir, home, home_len) == 0)
+	{
+		//Replace "/Users/username" to "~" and copy the rest
+		prompt_msg->pwd = ft_strjoin("~", &current_dir[home_len], 0);
+		free(home);
+		free(current_dir);
+	}
+	else
+		prompt_msg->pwd = current_dir;
 }
