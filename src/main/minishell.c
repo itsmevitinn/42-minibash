8f7f610b2bd8f1726997a8b81a5e6379cf96d079
Vitor Marcondes <vitorsergio111@gmail.com>
2022-12-01 15:52:36 -0300

    improve: readline dont work with only wt-spaces

--- src/main/minishell.c
+++ src/main/minishell.c
@@ -6,13 +6,14 @@
 /*   By: vsergio <vsergio@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/22 11:04:21 by vsergio           #+#    #+#             */
-/*   Updated: 2022/12/01 00:09:08 by Vitor            ###   ########.fr       */
+/*   Updated: 2022/12/01 15:38:17 by vsergio          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../include/minishell.h"
 
 int	g_running_process;
 
+static int	whitespace_checker(char *input);
 int main(void)
 {
 	char		*user_input;
@@ -21,15 +22,28 @@ int main(void)
 	while(42)
 	{
 		user_input = display_prompt();
-		add_history(user_input);
-		if (user_input && ft_strlen(user_input))
-			parse_input(user_input);
-		else if (!user_input)
+		if (!user_input)
 		{
+			//when ctrl + d is pressed
 			rl_clear_history();
 			return (0);
 		}
+		else if (!whitespace_checker(user_input))
+		{
+			add_history(user_input);
+			parse_input(user_input);
+		}
 		free(user_input);
 	}
-	rl_clear_history();
+}
+
+static int	whitespace_checker(char *input)
+{
+	int i;
+
+	i = 0;
+	while(input[i])
+		if (!ft_isspace(input[i++]))
+			return (0);
+	return (1);
 }
