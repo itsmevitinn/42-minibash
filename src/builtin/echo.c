7af96aa95b545a601a038835184fd9226988683b
Vitor Marcondes <vitorsergio111@gmail.com>
2022-12-01 18:47:19 -0300

    improve: handling -n parameter at echo

--- src/builtin/echo.c
+++ src/builtin/echo.c
@@ -6,21 +6,32 @@
 /*   By: Vitor <vsergio@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/30 22:49:26 by Vitor             #+#    #+#             */
-/*   Updated: 2022/11/30 23:37:50 by Vitor            ###   ########.fr       */
+/*   Updated: 2022/12/01 18:26:49 by vsergio          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../include/minishell.h"
 
 void	echo(char **splitted_cmd, int fd)
 {
-	char *content;
-	int i;
+	int word_index;
+	int trailing_newline;
 
-	content = splitted_cmd[1];
-	i = 0;
-	while(content[i])
-		write(fd, &content[i++], 1);
-	if (!ft_strncmp(splitted_cmd[1], "-n", 2))
+	trailing_newline = 1;
+	if (!ft_strncmp(splitted_cmd[1], "-n", 2) && ft_strlen(splitted_cmd[1]) == 2)
+	{
+		word_index = 2;
+		trailing_newline = 0;
+	}
+	else
+		word_index = 1;
+	while(splitted_cmd[word_index])
+	{
+		ft_putstr_fd(splitted_cmd[word_index], fd);
+		if (splitted_cmd[word_index + 1])
+			write(fd, " ", 1);
+		word_index++;
+	}
+	if (trailing_newline)
 		write(fd, "\n", 1);
 	// free_matrix(splitted_cmd);
 	return ;
