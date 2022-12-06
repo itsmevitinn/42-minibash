6ab6cb287c761c43a3da2a61e58eb2b6698d7fc6
Vitor Marcondes <vitorsergio111@gmail.com>
2022-12-06 10:12:09 -0300

    fix: env param at function

--- src/builtin/isbuiltin.c
+++ src/builtin/isbuiltin.c
@@ -6,7 +6,7 @@
 /*   By: vsergio <vsergio@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/28 11:59:16 by vsergio           #+#    #+#             */
-/*   Updated: 2022/12/05 23:10:57 by Vitor            ###   ########.fr       */
+/*   Updated: 2022/12/06 10:04:48 by vsergio          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../include/minishell.h"
@@ -26,7 +26,7 @@ int	isbuiltin(char **splitted_cmd)
 	else if (!ft_strncmp(splitted_cmd[0], "env", 3))
 		env(1);
 	else if (!ft_strncmp(splitted_cmd[0], "exit", 3))
-		ft_exit(23);
+		ft_exit(splitted_cmd);
 	else
 		return (0);
 	free_matrix(splitted_cmd);
