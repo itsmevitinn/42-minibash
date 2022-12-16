74b7a73618afa0912b7d89cd9a2c0495ebf9cc3c
itsmevitinn <vitorsergio111@gmail.com>
2022-12-16 19:16:41 -0300

    feat: redirect handled

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: Vitor <Vitor@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/29 10:10:05 by vsergio           #+#    #+#             */
-/*   Updated: 2022/12/15 18:00:28 by vsergio          ###   ########.fr       */
+/*   Updated: 2022/12/16 19:15:50 by Vitor            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,14 @@ void remove_chunk(char *cmd_line, int len);
 static void cmd_notfound(char *cmd_name);
 static void free_paths(char **paths, int i);
 static void build_lst_cmd(t_cmd_lst *lst_cmd, char *line);
-void	initialize_std_fd(t_cmd_lst *lst_cmd);
-void get_filename(t_cmd_lst *cmd, int offset, char type);
-void redirect_checker(t_cmd_lst *lst_cmd);
-int	check_file(t_cmd_lst *cmd, char *buffer, char type);
-int	check_infile(t_cmd_lst *cmd, char *buffer);
-int	check_outfile(t_cmd_lst *cmd, char *buffer);
+void initialize_std_fd(t_cmd_lst *lst_cmd);
+void get_filename(t_cmd_lst *cmd, char *line, char type);
+void redirect_checker(t_cmd_lst *cmd, char *line);
+int file_exists(t_cmd_lst *cmd, char *filename, int type);
+void get_sizes(char *line, int *chunk_size, int *file_size);
+void fill_filename(char *filename, char *start_file);
+void file_notfound(char *filename);
+void change_fd(t_cmd_lst *cmd, int file_fd, char type);
 
 void parse_input(char *line, t_var_lst *env_lst)
 {
@@ -31,7 +33,7 @@ void parse_input(char *line, t_var_lst *env_lst)
 
 	lst_cmd = NULL;
 	build_lst_cmd(lst_cmd, line);
-		// find_right_path(splitted_cmd);
+	// find_right_path(splitted_cmd);
 }
 
 static void build_lst_cmd(t_cmd_lst *lst_cmd, char *line)
@@ -47,19 +49,18 @@ static void build_lst_cmd(t_cmd_lst *lst_cmd, char *line)
 	initialize_std_fd(lst_cmd);
 	while (lst_cmd)
 	{
-		printf("line before:%s\n", lst_cmd->line);
-		redirect_checker(lst_cmd);
-		printf("line after: %s\n", lst_cmd->line);
+		printf("command: %s\n", lst_cmd->line);
+		redirect_checker(lst_cmd, lst_cmd->line);
 		lst_cmd->args = ft_split_quotes(lst_cmd->line, ' ');
 		print_cmd(lst_cmd->args);
-		is_builtin(lst_cmd, NULL);
+		// is_builtin(lst_cmd, NULL);
 		lst_cmd = lst_cmd->next;
 	}
 }
 
-void	initialize_std_fd(t_cmd_lst *lst_cmd)
+void initialize_std_fd(t_cmd_lst *lst_cmd)
 {
-	while(lst_cmd)
+	while (lst_cmd)
 	{
 		lst_cmd->input = 0;
 		lst_cmd->output = 1;
@@ -67,91 +68,102 @@ void	initialize_std_fd(t_cmd_lst *lst_cmd)
 	}
 }
 
-void redirect_checker(t_cmd_lst *cmd)
+void redirect_checker(t_cmd_lst *cmd, char *line)
 {
-	int offset;
-
-	offset = 0;
-	while (cmd->line[offset])
+	while (*line)
 	{
-		if (cmd->line[offset] == '<' || cmd->line[offset] == '>')
-			get_filename(cmd, offset, cmd->line[offset]);
-		offset++;
+		if (*line == '<' || *line == '>')
+			get_filename(cmd, line, *line);
+		else
+			line++;
 	}
 }
 
-void get_filename(t_cmd_lst *cmd, int offset, char type)
+void get_filename(t_cmd_lst *cmd, char *line, char type)
 {
-	char *buffer;
-	int filename;
-	int len;
-	int start_chunk;
-	int buffer_offset;
+	char *filename;
+	int chunk_size;
+	int file_size;
 
-	start_chunk = offset;
-	buffer_offset = 0;
-	len = 0;
-	offset++;
-	while (cmd->line[offset] && ft_isspace(cmd->line[offset]))
-		offset++;
-	filename = offset;
-	while (cmd->line[offset] && !ft_isspace(cmd->line[offset]))
-	{
-		offset++;
-		len++;
-	}
-	buffer = malloc(sizeof(char) * (len + 1));
-	while (cmd->line[filename] && !ft_isspace(cmd->line[filename]))
-		buffer[buffer_offset++] = cmd->line[filename++];
-	buffer[buffer_offset] = '\0';
-	if (check_file(cmd, buffer, type))
-		remove_chunk(&cmd->line[start_chunk], offset - 1);
+	chunk_size = 0;
+	file_size = 0;
+	get_sizes(line, &chunk_size, &file_size);
+	filename = malloc(sizeof(char) * (file_size + 1));
+	fill_filename(filename, line);
+	if (file_exists(cmd, filename, type))
+		remove_chunk(line, chunk_size);
 }
 
-int	check_file(t_cmd_lst *cmd, char *buffer, char type)
+void file_notfound(char *filename)
 {
-	if (type == '<' && check_infile(cmd, buffer))
-		return (1);
-	else if (type == '>' && check_outfile(cmd, buffer))
-		return (1);
-	return (0);
+	ft_putstr_fd("bash: ", 2);
+	ft_putstr_fd(filename, 2);
+	ft_putstr_fd(": No such file or directory\n", 2);
+	exit(1);
 }
 
-int	check_infile(t_cmd_lst *cmd, char *buffer)
+void fill_filename(char *filename, char *line)
 {
-	int infile;
-	
-	infile = open(buffer, O_RDONLY, 0666);
-	if (infile == -1)
-		return (0);
-	else
+	char *temp;
+
+	temp = filename;
+	line++;
+	while (*line && ft_isspace(*line) && *line != '<' && *line != '>')
+		line++;
+	while (*line && !ft_isspace(*line) && *line != '<' && *line != '>')
+		*temp++ = *line++;
+	*temp = '\0';
+}
+
+void get_sizes(char *line, int *chunk_size, int *file_size)
+{
+	line++;
+	while (*line && ft_isspace(*line) && *line != '<' && *line != '>')
 	{
-		printf("new input fd from: %s\n", buffer);
-		cmd->input = infile;
-		printf("input fd: %d\n", cmd->input);
+		(*chunk_size)++;
+		line++;
 	}
+	while (*line && !ft_isspace(*line) && *line != '<' && *line != '>')
+	{
+		line++;
+		(*chunk_size)++;
+		(*file_size)++;
+	}
+}
+
+int file_exists(t_cmd_lst *cmd, char *filename, int type)
+{
+	int file_fd;
+
+	file_fd = 0;
+	if (type == '<')
+		file_fd = open(filename, O_RDONLY, 0666);
+	else if (type == '>')
+		file_fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0666);
+	if (file_fd == -1)
+		file_notfound(filename);
+	change_fd(cmd, file_fd, type);
 	return (1);
 }
 
-int	check_outfile(t_cmd_lst *cmd, char *buffer)
+void change_fd(t_cmd_lst *cmd, int file_fd, char type)
 {
-	int outfile;
-
-	outfile = open(buffer, O_CREAT | O_RDWR | O_TRUNC, 0666);
-	if (outfile == -1)
-		return (0);
-	else
+	if (type == '<')
 	{
-		printf("new output fd from: %s\n", buffer);
-		cmd->output = outfile;
-		printf("output fd: %d\n", cmd->output);
+		if (cmd->input != 0)
+			close(cmd->input);
+		cmd->input = file_fd;
 	}
-	return (1);
+	else if (type == '>')
+	{
+		if (cmd->output != 1)
+			close(cmd->input);
+		cmd->output = file_fd;
+	}
 }
 
 void remove_chunk(char *line, int len)
 {
-	printf("size: %i\n", len);
 	char *aux;
 
 	while (len >= 0)
@@ -173,11 +185,11 @@ void remove_chunk(char *line, int len)
 // 	all_cmds = ft_split_quotes(user_input, '|');
 // 	build_lst_cmd(all_cmds);
 // 	return (NULL);
-	// cmd = ft_split_quotes(cmd[0], ' ');
-	// while (cmd[args++])
-	// Cut the quotes at the edges of each arguments
-	// cmd[args] = ft_strtrim_edges(cmd[args], "'\"");
-	// return (cmd);
+// cmd = ft_split_quotes(cmd[0], ' ');
+// while (cmd[args++])
+// Cut the quotes at the edges of each arguments
+// cmd[args] = ft_strtrim_edges(cmd[args], "'\"");
+// return (cmd);
 // }
 
 void find_right_path(char **splitted_cmd)
