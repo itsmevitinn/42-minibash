f2be848c0ec0af8ab9aa97d50ff4275899e8eabd
Vitor Sergio Marcondes Costa <vitor@Vitors-MacBook-Air.local>
2022-12-27 02:32:07 -0300

    feat: echo heredoc exit status done

--- src/builtin/echo.c
+++ src/builtin/echo.c
@@ -3,19 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: Vitor <Vitor@student.42.fr>                +#+  +:+       +#+        */
+/*   By: vsergio <vsergio@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/30 22:49:26 by Vitor             #+#    #+#             */
-/*   Updated: 2022/12/24 17:48:58 by Vitor            ###   ########.fr       */
+/*   Updated: 2022/12/27 02:26:02 by vsergio          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
+int check_heredoc(t_cmd_lst *cmd);
+int execute_heredoc(t_cmd_lst *cmd);
 
-void echo(t_cmd_lst *cmd, t_cmd_info *data)
+int echo(t_cmd_lst *cmd, t_cmd_info *data)
 {
 	int status;
 
+	if (!check_heredoc(cmd))
+		return (0);
 	cmd->pid = fork();
 	if (!cmd->pid)
 	{
@@ -24,9 +28,6 @@ void echo(t_cmd_lst *cmd, t_cmd_info *data)
 
 		content_index = -1;
 		trailing_newline = 1;
-		restore_sigint();
-		if (cmd->delimiter)
-			get_heredoc_input(cmd);
 		if (!cmd->args[1])
 		{
 			ft_putstr_fd("\n", cmd->output);
@@ -54,8 +55,32 @@ void echo(t_cmd_lst *cmd, t_cmd_info *data)
 	if (cmd->id < (data->qty - 1))
 		close(data->pipes[cmd->id][1]);
 	waitpid(cmd->pid, &status, 0);
+	g_exit_status = WEXITSTATUS(status);
+	return (1);
+}
+
+int check_heredoc(t_cmd_lst *cmd)
+{
+	if (cmd->delimiter)
+		if (!execute_heredoc(cmd))
+			return (0);
+	return (1);
+}
+
+int execute_heredoc(t_cmd_lst *cmd)
+{
+	int status;
+	int heredoc_pid;
+
+	heredoc_pid = fork();
+	if (!heredoc_pid)
+	{
+		restore_sigint();
+		get_heredoc_input(cmd);
+		exit(0);
+	}
+	waitpid(heredoc_pid, &status, 0);
 	if (!WIFEXITED(status))
-		return;
-	else
-		g_exit_status = WEXITSTATUS(status);
+		return (0);
+	return (1);
 }
