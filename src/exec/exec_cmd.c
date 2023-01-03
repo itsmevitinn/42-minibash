cfdfabf7f2a8508b1480c3038b84efb5f7981a08
Vitor Sergio Marcondes Costa <vitor@Vitors-MacBook-Air.local>
2023-01-02 21:13:59 -0300

    feat: inexistent file error handled

--- src/exec/exec_cmd.c
+++ src/exec/exec_cmd.c
@@ -6,18 +6,20 @@
 /*   By: vsergio <vsergio@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/28 11:59:16 by vsergio           #+#    #+#             */
-/*   Updated: 2022/12/30 01:17:59 by vsergio          ###   ########.fr       */
+/*   Updated: 2023/01/02 21:13:05 by vsergio          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-static int find_cmd_type(t_cmd_lst *cmd, t_cmd_info *data, t_var_lst **env_lst);
+static int	error_found(t_cmd_lst *cmd);
+static int	find_cmd_type(t_cmd_lst *cmd, t_cmd_info *data,
+				t_var_lst **env_lst);
 
-void exec_cmds(t_cmd_info *data, t_var_lst **env_lst)
+void	exec_cmds(t_cmd_info *data, t_var_lst **env_lst)
 {
-	t_cmd_lst *cmd;
-	int pipes_qty;
+	t_cmd_lst	*cmd;
+	int			pipes_qty;
 
 	cmd = data->lst_cmd;
 	pipes_qty = data->qty - 1;
@@ -27,7 +29,7 @@ void exec_cmds(t_cmd_info *data, t_var_lst **env_lst)
 		{
 			if (cmd->id < pipes_qty)
 				close(data->pipes[cmd->id][1]);
-			break;
+			break ;
 		}
 		if (cmd->id < pipes_qty)
 			close(data->pipes[cmd->id][1]);
@@ -35,9 +37,9 @@ void exec_cmds(t_cmd_info *data, t_var_lst **env_lst)
 	}
 }
 
-static int find_cmd_type(t_cmd_lst *cmd, t_cmd_info *data, t_var_lst **env_lst)
+static int	find_cmd_type(t_cmd_lst *cmd, t_cmd_info *data, t_var_lst **env_lst)
 {
-	if (!*cmd->args)
+	if (error_found(cmd))
 		return (1);
 	if (is_builtin(cmd->args[0]))
 	{
@@ -50,4 +52,20 @@ static int find_cmd_type(t_cmd_lst *cmd, t_cmd_info *data, t_var_lst **env_lst)
 			return (0);
 	}
 	return (1);
+}
+
+static int	error_found(t_cmd_lst *cmd)
+{
+	if (cmd->input == -1)
+	{
+		ft_putstr_fd("bash: ", 2);
+		ft_putstr_fd(cmd->filename, 2);
+		ft_putstr_fd(": ", 2);
+		perror(NULL);
+		return (1);
+	}
+	//empty command, just go to the next one
+	else if (!*cmd->args)
+		return (1);
+	return (0);
 }
